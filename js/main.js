a56ea6801471d60f78be84e52ed99669979b7e99
Matt Kistler <Matt.A.Kistler@gmail.com>
2016-04-03 12:26:03 -0500

    first draft

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+    $("#o").hover(function() {
+     $("#music").css("display", "inline-block");
+ }, function() {
+     $("#music").css("display", "none");
+ });
+    $("#l").hover(function() {
+     $("#shows").css("display", "inline-block");
+ }, function() {
+     $("#shows").css("display", "none");
+ });
+    $("#d").hover(function() {
+     $("#store").css("display", "inline-block");
+ }, function() {
+     $("#store").css("display", "none");
+ });
+    $("#s").hover(function() {
+     $("#contact").css("display", "inline-block");
+ }, function() {
+     $("#contact").css("display", "none");
+ });
+    $("#e").hover(function() {
+     $("#social").css("display", "inline-block");
+ }, function() {
+     $("#social").css("display", "none");
+ });
+});
